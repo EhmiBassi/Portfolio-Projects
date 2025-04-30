e2c515ba55a3b029cc66432ec16ad1d5081c4037
Emediong Bassey <remexbassi@gmail.com>
2025-04-30 12:01:47 +0100

    updated

--- Marketing Campaign Analysis/combined_sql_analysis.sql
+++ Marketing Campaign Analysis/combined_sql_analysis.sql
@@ -6,22 +6,19 @@ FROM marketing_campaign_dataset;
 /*1. Calculate Total Impressions for Each Campaign 
 Expected Output: A table with campaign_id and total impressions. */
 
-SELECT Campaign_ID, Campaign_Type, SUM(Impressions) AS Total_Impressions
+SELECT Campaign_ID, SUM(Impressions) AS Total_Impressions
 FROM marketing_campaign_dataset
-GROUP BY Campaign_ID, Campaign_Type
+GROUP BY Campaign_ID
 ORDER BY Total_Impressions DESC;
-
 
 
 
 /*2. Identify the Campaign with the Highest ROI
 Expected Output: A single row with campaign_id, company, and roi. */
 
-SELECT TOP (1) Campaign_ID, Company, SUM(ROI) AS ROI
+SELECT TOP (1) Campaign_ID, Company, ROI
 FROM marketing_campaign_dataset
-GROUP BY Campaign_ID, Company
 ORDER BY ROI DESC;
-
 
 
 
@@ -35,7 +32,6 @@ ORDER BY Total_Impressions DESC;
 
 
 
-
 /*4. Calculate Average Engagement Score by Target Audience
 Expected Output: A table with target_audience and avg engagement score. */
 
@@ -45,30 +41,23 @@ GROUP BY Target_Audience;
 
 
 
-
 /*5. Calculate the Overall CTR (Click-Through Rate)
 Expected Output: A single value for the overall CTR. */
 
 --CTR = CLICKS/IMPRESSIONS.
 --Multiplying Clicks by 1.0 ensures it returns a decimal value
 
-SELECT ROUND(SUM(Clicks * 1.0 / Impressions), 2) AS Overall_Click_Through_Rate  
+SELECT CEILING(SUM(Clicks * 1.0 / Impressions)) AS Overall_Click_Through_Rate  
 FROM marketing_campaign_dataset;
-
 
 
 
 /*6. Find the Most Cost-Effective Campaign
 Expected Output: A table with campaign_id, company, and cost per conversion. */
 
---Format value to currency(%)
-
-SELECT TOP (1) Campaign_ID, Company, FORMAT(Sum(Acquisition_Cost / ConversionRate), 'C', 'en-US') AS Cost_per_conversion 
+SELECT TOP (1) Campaign_ID, Company, CEILING((Acquisition_Cost / ConversionRate)) AS Cost_per_conversion 
 FROM marketing_campaign_dataset
-GROUP BY Campaign_ID, Company
-ORDER BY Sum(Acquisition_Cost / ConversionRate);
-
-
+ORDER BY Cost_per_conversion ASC;
 
 
 /*7. Find Campaigns with CTR Above a Threshold
@@ -78,14 +67,18 @@ Expected Output: A table with campaign_id, company, and ctr. */
 --Multiply by 100 to get the CTR in percentage and round it up to 2 decimal places
 
 SELECT 
-Campaign_ID, 
-Company, 
-FORMAT(ROUND((Clicks * 1.0 / Impressions) * 100, 2), 'N2') + '%'--format it to get the value in percentage
-AS CTR_Percentage
-FROM marketing_campaign_dataset
-WHERE ROUND((Clicks * 1.0 / Impressions) * 100, 2) > 40 --We make 40 our Threshold value
-ORDER BY CTR_Percentage DESC;
-
+    Campaign_ID, 
+    Company, 
+    CTR
+FROM (
+    SELECT 
+        Campaign_ID, 
+        Company, 
+        ROUND((Clicks * 1.0 / Impressions) * 100, 2) AS CTR
+    FROM marketing_campaign_dataset
+) AS sub
+WHERE CTR > 40 -- 40 being our Threshold value
+ORDER BY CTR ASC;
 
 
 
